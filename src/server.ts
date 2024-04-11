912c1c97d3929eb17852356bc9ba1e417aeaa180
Jhon Cruz <jhonskaz@gmail.com>
2024-04-11 11:26:47 -0300

    Adiciona migrations e primeiras consultas ao banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
 import { knex } from "./database";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+  // const transactions = await knex("transactions")
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: "Transação de teste",
+  //     amount: 1000,
+  //   })
+  //   .returning("*");
+  // const transactions = knex("transactions").select("*");
+  // const transactions = knex("transactions").where("amount", 500).select("*");
+  const transactions = knex("transactions").where("amount", 1000).select("*");
 
-  return tables;
+  return transactions;
 });
 
 app
