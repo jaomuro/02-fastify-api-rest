bd66bdd1b9027303a9d76cbfa3961630187c972f
Jhon Cruz <jhonskaz@gmail.com>
2024-04-11 11:44:36 -0300

    Adiciona variáveis de ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,12 @@
 import { knex as setupKnex, Knex } from "knex";
+import "dotenv/config";
+
+if (!process.env.DATABASE_URL)
+  throw new Error("DATABASE_URL not defined in .env");
 
 export const config: Knex.Config = {
   client: "sqlite",
-  connection: { filename: "./db/app.db" },
+  connection: { filename: process.env.DATABASE_URL },
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,16 @@ import { knex } from "./database";
 const app = fastify();
 
 app.get("/hello", async () => {
-  // const transactions = await knex("transactions")
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: "Transação de teste",
-  //     amount: 1000,
-  //   })
-  //   .returning("*");
+  const transactions = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de teste",
+      amount: 1000,
+    })
+    .returning("*");
   // const transactions = knex("transactions").select("*");
   // const transactions = knex("transactions").where("amount", 500).select("*");
-  const transactions = knex("transactions").where("amount", 1000).select("*");
+  // const transactions = knex("transactions").where("amount", 1000).select("*");
 
   return transactions;
 });
