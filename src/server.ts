c33bd7ffc77b05a39a37a2177b821d3143c5ef6a
Jhon Cruz <jhonskaz@gmail.com>
2024-04-11 16:26:21 -0300

    Adiciona plugin de rotas do fastify

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,10 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
-import { knex } from "./database";
-import { env } from "../env";
+import { env } from "./env";
+import { transactionsRoutes } from "./transactions/transactions";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  // const transactions = await knex("transactions")
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: "Transação de teste",
-  //     amount: 1000,
-  //   })
-  //   .returning("*");
-  // const transactions = knex("transactions").select("*");
-  // const transactions = knex("transactions").where("amount", 500).select("*");
-  const transactions = knex("transactions").where("amount", 1000).select("*");
-
-  return transactions;
-});
+app.register(transactionsRoutes);
 
 app
   .listen({
