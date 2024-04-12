f5dbf08e31544586bc62f932dc457a31a4e6698b
Jhon Cruz <jhonskaz@gmail.com>
2024-04-12 16:48:24 -0300

    Adiciona testes - busca um recurso específico, busca resumo

--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -1,7 +1,7 @@
-import { afterAll, beforeAll, it, describe, expect } from "vitest";
+import { afterAll, beforeAll, it, describe, expect, afterEach } from "vitest";
 import request from "supertest";
 import { app } from "../app";
-
+import { execSync } from "node:child_process";
 describe("Transactions routes", () => {
   beforeAll(async () => {
     await app.ready();
@@ -9,6 +9,11 @@ describe("Transactions routes", () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  afterEach(() => {
+    execSync("npm run knex migrate:rollback --all");
+    execSync("npm run knex migrate:latest");
   });
 
   it("should be able to create an new transaction", async () => {
@@ -40,5 +45,66 @@ describe("Transactions routes", () => {
     expect(transactionListResponse.body.transactions).toEqual([
       expect.objectContaining({ title: "new transaction", amount: 100 }),
     ]);
+  });
+
+  it("should be able to get a specific transaction", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "new transaction",
+        amount: 100,
+        type: "credit",
+      });
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const transactionListResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    const transactionId = transactionListResponse.body.transactions[0].id;
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: "new transaction",
+        amount: 100,
+        id: transactionId,
+      })
+    );
+  });
+
+  it("should be able to get the summary", async () => {
+    const createTransactionCreditResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "credit transaction",
+        amount: 200,
+        type: "credit",
+      });
+
+    const cookies = createTransactionCreditResponse.get("Set-Cookie");
+
+    await request(app.server)
+      .post("/transactions")
+      .set("Cookie", cookies)
+      .send({
+        title: "debit transaction",
+        amount: 100,
+        type: "debit",
+      });
+
+    const getSummaryTransactionsResponse = await request(app.server)
+      .get("/transactions/summary")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(getSummaryTransactionsResponse.body.summary).toEqual({
+      amount: 100,
+    });
   });
 });
